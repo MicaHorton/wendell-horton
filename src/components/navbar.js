7e239f63f0d39a7c3ed2e73eb734e98e0faf07f0
Mica Horton <mica.e.horton@gmail.com>
2020-09-11 22:20:23 -0700

    Fix fontawesome dependency issue (2)

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import 'font-awesome/css/font-awesome.min.css';
+
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Navbar extends Component { 
     constructor (props) {
@@ -14,13 +16,20 @@ export default class Navbar extends Component {
     }
 
     render () {
+        let icon;
+        if (this.state.overlay) {   
+            icon = <FontAwesomeIcon className='fa-times' icon={faTimes} />
+        } else {
+            icon = <FontAwesomeIcon className ='fa-bars' icon={faBars} />
+        }
+
         return (
             <>
             <div className={` overlay ${this.state.overlay && 'overlay-active'} `}></div>
      
             <div className='header'>
-                <button className='toggle' onClick={() => this.toggleNav()}>
-                    <i className={` ${!this.state.overlay && 'fa fa-bars'}  ${this.state.overlay && 'fa fa-times'} ${this.state.overlay && 'overlay-active'}`}></i>
+                <button className='toggle' onClick={() => this.toggleNav()}> 
+                    {icon}
                 </button>
                 <h1>Wendell Horton</h1>           
             </div>
@@ -32,12 +41,16 @@ export default class Navbar extends Component {
             </nav>
 
             
-            </>
-      
-             
-        
+            </>  
 
         )
 
     }
 }
+
+/* 
+import 'font-awesome/css/font-awesome.min.css'; 
+<i className={` ${!this.state.overlay && 'fa fa-bars'}  ${this.state.overlay && 'fa fa-times'} ${this.state.overlay && 'overlay-active'}`}></i>
+
+
+*/
