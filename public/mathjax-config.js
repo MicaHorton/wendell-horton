7e86ea7b2ca90184599094b09bcf56c93b7e9d5e
Mica Horton <mica.e.horton@gmail.com>
2020-09-18 15:34:44 -0700

    Finally fix MathJax problem
    mathjax-config.js has to be in the public folder for some reason

---
+++ public/mathjax-config.js
@@ -0,0 +1,25 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  svg: {
+    fontCache: 'global'
+  }
+};
+
+
+/*
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  chtml: {
+      scale: 10
+  },
+  svg: {
+    fontCache: 'global'
+  }
+};*/
+
+
+
