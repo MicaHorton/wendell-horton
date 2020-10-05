e11101bd91013df16c339e989ec894d13bc707f6
Mica Horton <mica.e.horton@gmail.com>
2020-10-05 10:05:06 -0700

    Add introduction

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,30 +4,30 @@ export default class Intro extends Component {
     render () {
         return (
             <div className='content'>
-                <h1>About</h1>
+                <h1>Introduction</h1>
                 <p>
-                    Impedit suscipit et pariatur veniam. Qui et nulla repudiandae ullam consequatur ea. 
-                    Officiis dolore sed non sit amet aut. Explicabo sed harum non aspernatur quia et deleniti. 
-                    Voluptate animi ut cupiditate aut saepe doloremque. Consectetur sit aspernatur quia eos consequuntur similique.
+                    Wendell Horton plasma physics  career began as a graduate student at the University of California in San Diego in 1963. UCSD  was a new University of California campus growing out of the Scrips Institute of Oceanography. The San Diego University had just started with some famous scientists both in astrophysics …the XX and YY Burbidges .. and in plasma physics..Professor Marshal Rosenbluth and Professor Norman Rostoker and nuclear physics --Maria Mayer.  I was lucky to find Professor Rosenbluth interested in my background of knowning well computer simulation methods from my early interest and hobby work first in Austin High School and then at the Univ of Texas in electronics and computer programming -which at that time was a tedious procedure with punched cards. The problem that Rosenbluth gave me was to research the new experiments that showed the coupling of the Langmuir plasma waves with the electromagnetic radiations waves.
+                    Surprising results were just published from by Buchsbaum Refs [see my publication Horton 196x] of radiation at high harmonics of the electron cyclotron frequency in a plasma of only a few electron volts temperature. Theory at that time required high kilovolt  temperatures plasma temperatures to produce torhis radiation. From this first research 
                 </p>
                 <p>
-                    Blanditiis eos non sint harum sit animi nihil et. Nesciunt incidunt nihil dolore ut. 
-                    Occaecati suscipit et minus architecto qui est velit et. Atque rerum numquam ut earum qui deserunt facilis. 
-                    Autem voluptatem ipsam recusandae harum sit explicabo distinctio quos.
+                    I published a new theory that showed in the cylindrical laboratory vacuum vessel a plasmas at modest electron  temperature could produce up to ten harmonics electron cyclotron waves.  The theory was developed by taking keeping the radial density and magnetic field profiles of the plasma produced and confined in the metal cylinder. The metal wall boundary conditions on the confined plasma thus made the both density and the magnetic field have radial profiles that lead to the coupling of the plasma waves.  The walls and non-uniformity of the plasma required a  new derivation of the wave equations for both the electromagnetic and Langmuir waves. The new wave equations showed that there is radial layer in the plasma
+                    where the wave numbers at the fixed wave frequency in the cylinder would conincide for the transverse electromagnetic wave and the electrostatic plasma Langmuir wave. A new analytic calculation followed up by numerical calculations showed that this resonant layer — now called the upper hybrid resonance layer—gives rise to the fixed frequency waves transferring energy across the two wave polarizations. The plasma Langmuir wave couples through the resonance layer to the transverse electromagnetic wave and vice-a-versa.
                 </p>
                 <p>
-                    Distinctio vero saepe ut sed itaque optio. Molestias unde unde quasi voluptatem dolorum quo iste. 
-                    Nesciunt id commodi aut. Rem illum unde eaque. Modi commodi eos non qui magni reprehenderit iste. 
-                    Molestiae unde ea quia quisquam et.
+                    This wave coupling at the upper hybrid resonance layer is now a critical technique used in fusion plasmas to heat the electrons to fusion temperatures. First used in early tokamak experiments and now designed for the critical fusion power experiments in  ITER the RF waves launched by complex antennas on the vacuum chamber walls are used to heat the electrons component of the plasma to the
+                    tens of kilovolts electron temperatures required for fusion power plasmas. The design of the RH antennas is an advanced technology now that is to achieve and maintain the tens of Kev electron temperatures[ in many plasma fusion experiments and is an active area of research for new plasma graduate students. A related area that uses mode conversion and coupling across the lower hybrid resonance layer that occurs for plasma wave crossing the ion cyclotron freuency in the plasma is also of critical importance fo fusion plasma physics machines. The understanding and closely related theory is important in space plasma physics particularly at the E-layer of the Earth’s atmosphere at an altitudes between 10 - 15,000km. This coupling here causes strong distortions in the Global Positioning Satellite System GPSS data that is important both for civilian and defense communications. Thus, the study and research of radio frequency waves RFW in plasma is critically important for new researchers in plasma physics. 
                 </p>
                 <p>
-                    Rem ratione eos quam. Quidem ex quia dignissimos delectus nostrum nostrum est quibusdam. 
-                    Animi id voluptas totam voluptas blanditiis ullam non vero. Inventore inventore est voluptatem sunt eligendi qui corporis.
-                    Hic provident at voluptatibus expedita aut blanditiis ut et. Et odit dignissimos eligendi enim provident dolores id totam. 
-                    Doloremque laborum unde explicabo eum impedit distinctio. Enim labore unde cupiditate nihil.
+                    An example of the discover of the mode conversion processing astrophysical plasmas came from resolving the first mysterious measurements of electromagnetic radiation at the cyclotron harmonics from Jupiter and subseuently other planets with strong magnetic fields.
+                    Now there is clear understanding and large set of publications on how the dipolar magnetic field structure of the planets and stars creates a zone where the electrons spiraling down the increasing strong magnetic fields radiate  electromagnetic electron cyclotron waves at harmonics of the electron cyclotron sequences. This radiation is now important for identifying extra solar systems planets.  Where stars in other solar systems in the universe have planets like our  planet Jupiter with strong magnetic fields have surrounding plasmas that emit strong electromagnetic plasma electron cyclotron waves at high harmonics of the electron cyclotron frequency. This cyclotron radiation is now an important tool for identifying extra solar systems planets.  Where many stars have been identified  like our sun have planets -- like our Jupiter --with strong magnetic fields surrounded by plasmas that emit intense electron cyclotron radiation.
                 </p>
+                <p>
+                    External driven ECH heating is of fundamental importance for reaching state state fusion plasmas and is actively developed in the International fusion plasmas in both the tokamak and stellarator machines for fusion power plasmas and power plant designs. Refs.
+                </p>
+               
             </div>
             
         )
     }
 }
+
