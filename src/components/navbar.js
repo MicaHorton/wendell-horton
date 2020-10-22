aa8b6901485d64ca0a566c021da224744c3a31e1
Mica Horton <mica.e.horton@gmail.com>
2020-10-21 18:03:26 -0700

    Fix new chapter errors

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -38,9 +38,17 @@ export default class Navbar extends Component {
             <nav className={` nav ${this.state.overlay && 'overlay-active'} `}>
                 <Link className='nav-item' to='/' onClick={() => this.toggleNav()}>Introduction</Link>
                 <Link className='nav-item' to='/prologue' onClick={() => this.toggleNav()}>Prologue</Link>
+                <Link className='nav-item' to='/chapter1' onClick={() => this.toggleNav()}>Chapter 1</Link>
+                <Link className='nav-item' to='/chapter2' onClick={() => this.toggleNav()}>Chapter 2</Link>
+                <Link className='nav-item' to='/chapter3' onClick={() => this.toggleNav()}>Chapter 3</Link>
                 <Link className='nav-item' to='/chapter4' onClick={() => this.toggleNav()}>Chapter 4</Link>
                 <Link className='nav-item' to='/chapter5' onClick={() => this.toggleNav()}>Chapter 5</Link>
                 <Link className='nav-item' to='/chapter6' onClick={() => this.toggleNav()}>Chapter 6</Link>
+                <Link className='nav-item' to='/chapter7' onClick={() => this.toggleNav()}>Chapter 7</Link>
+                <Link className='nav-item' to='/chapter8' onClick={() => this.toggleNav()}>Chapter 8</Link>
+                <Link className='nav-item' to='/chapter9' onClick={() => this.toggleNav()}>Chapter 9</Link>
+                <Link className='nav-item' to='/chapter10' onClick={() => this.toggleNav()}>Chapter 10</Link>
+                <Link className='nav-item' to='/chapter11' onClick={() => this.toggleNav()}>Chapter 11</Link>
             </nav>
 
             
